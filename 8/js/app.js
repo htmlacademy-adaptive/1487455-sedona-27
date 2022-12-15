fb2ae3cfcb05367848aa06fb73c335d2a467e377
Keks <keks@htmlacademy.ru>
2022-12-15 20:24:25 +0300

    :heavy_check_mark: Сборка #8

---
+++ 8/js/app.js
@@ -0,0 +1,11 @@
+let navigationButton = document.querySelector('.navigation__button');
+let navigationList = document.querySelector('.navigation__list');
+
+document.querySelector(".navigation--no-js").classList.remove("navigation--no-js");
+
+navigationButton.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  navigationButton.classList.toggle('navigation__button--close');
+  navigationList.classList.toggle('navigation__list--opened');
+});
